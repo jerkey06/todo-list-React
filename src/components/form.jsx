f8e726d1ccd18bbaf82a41f7c4baf8666fda0afa
jerkey06 <ponceemiliano44@gmail.com>
2024-07-23 20:26:15 -0600

    form cambiado

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const form = () => {
+const Form = () => {
   return (
     <form className='mb-4 font-primarty w-full'>
         <input type="text" className='unline-none bg-transparent border border-gray-500 p-4 w-[300px] text-white mb-8 rounded placeholder:text-gray-300' placeholder='What tasks will you do today?'/>
-        <button className='bg-blue-500 text-white p-4 rounded'>Add Task</button>
+        <button className='bg-gray-700 border-none p-2 text-white cursor-pointer rounded m1-2'>Add Task</button>
     </form>
     )
 }
 
-export default form
+export default Form
--- src/components/todoList.jsx
+++ src/components/todoList.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const todoList = () => {
   return (
-    <div>todoList</div>
+    <div>
+        <form/>
+    </div>
   )
 }
 
